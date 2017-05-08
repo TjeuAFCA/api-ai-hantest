77878dd6ac71c66b1c2331a66169c79bd368a201
thomaszee <thomasvdzee@gmail.com>
2017-05-08 13:48:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -28,27 +28,14 @@ restService.post('/webhook', function (req, res) {
                 var parameters = result.parameters;
                 var zone = parameters["shipping-zone"];
                 
-                 // try {
-                      sql.connect('mssql://thomaszee:Korilu5!@isaschatbot.database.windows.net/isaschatbot', err => {
-                          
-    new sql.Request().query('select FirstName from Student where id = 1', (err, result) => {
-        console.dir(result)
-    })
-
-})
-
-sql.on('error', err => {
-    console.log(err);
-})
-
-                                                                
-                 /*    const pool = sql.connect('mssql://thomaszee:Korilu5!@isaschatbot.database.windows.net/isaschatbot')
+                  try {                                            
+                   const pool = sql.connect('mssql://thomaszee:Korilu5!@isaschatbot.database.windows.net/isaschatbot')
                      const sqlResult = sql.query('select FirstName from Student where id = 1');
                       console.log('sqlresult below');
                       console.log(sqlResult);
                   } catch (err) {
                         console.log('sqlerr: ' + err);
-                  } */
+                  } 
 
                 var cost = { 'Europe': 100, 'North America': 200, 'South America': 300, 'Asia': 400, 'Africa': 500 }
 
