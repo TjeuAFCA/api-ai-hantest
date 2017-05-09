11da20a01c509f6326187c8f6c6ad417d652bcf2
TjeuAFCA <tjeujansen@hotmail.com>
2017-05-09 11:52:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,7 @@ restService.use(bodyParser.json());
 
 
    function getQuery(query) {
-    var conn = new sql.Connection(config);
-    
-    conn.connect().then(function () {
+    sql.connect(config).then(function () {
         var req = new sql.Request(conn);
         req.query(query).then(function (recordset) {
             console.log(recordset);
