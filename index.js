09cdb0e8ec8f4504c6192bc7229eedef946d4b2d
TjeuAFCA <tjeujansen@hotmail.com>
2017-05-15 15:08:39 +0200

    Commit

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function executeQuery(query, callback) {
 
 function getResultText(res, text) {
     console.log('res =  ');
-    console.log(res.socket);
+    console.log(res.socket.body);
     return res.json({
         speech: text,
         displayText: text,
