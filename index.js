52935654cd1cab01b7a877df8aa0aec7d0b6f23f
TjeuAFCA <tjeujansen@hotmail.com>
2017-05-15 15:00:07 +0200

    Commit

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function executeQuery(query, callback) {
 }
 
 function getResultText(res, text) {
-    console.log('res = ');
+    console.log('res =  ');
     console.log(res);
     return res.json({
         speech: text,
