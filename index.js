b954766fecb449f1711a9e46f9edb4c191bf81be
TjeuAFCA <tjeujansen@hotmail.com>
2017-05-16 15:28:44 +0200

    Commit

--- index.js
+++ index.js
@@ -158,7 +158,10 @@ restService.post('/webhook', function (req, res) {
                     executeQuery("SELECT e.DateTime, e.Name FROM Exam e INNER JOIN Class c ON e.Class = c.Id INNER JOIN Student s ON c.Id = s.Class INNER JOIN Subject su ON su.Id = e.Subject WHERE s.Id = 1 AND su.Name = '" + vakken + "'",
                         function (data) {
                             if (data.recordset[0]) {
-                                speech = "Je toets (" +  data.recordset[0].Name + ") voor " + vakken + " is op " + Date.parse(data.recordset[0].DateTime);
+                                var time = Date.parse(data.recordset[0].DateTime);
+                                var Date = new Date(time);
+                                var examDate = Date.getDate() + "-" + Date.getMonth() + "-" + Date.getFullYear() + " om " + Date.getHours() + ":" + Date.getMinutes() + " uur";
+                                speech = "Je toets (" +  data.recordset[0].Name + ") voor " + vakken + " is op " + examDate;
                                 return getResultText(res, speech);
                             }
                             else {
